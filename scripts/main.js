abd7ab571b72b13775edc5f8206de79f7ac87294
Herifeno Fahasoavana <hei.fahasoavana@gmail.com>
2022-05-06 10:45:26 +0300

    fix:main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 //importation des modules
 const prompt = require("prompt-sync")();
-const fonction = require("./chiffrement");
+const chiffrement = require("./chiffrement");
 
 //Test du premier fonction chiffrement d'une seule caractère
 const caractere = prompt("Saisir un caractère :");
-console.log(fonction.chiffrerCaractere(caractere));
+console.log(chiffrement.chiffrerCaractere(caractere));
 
-//Test du deuxième fonction chiffrement d'une chaîne de caractère
+//Test du deuxième chiffrement chiffrement d'une chaîne de caractère
 const chaine = prompt("Saisir une chaîne :");
-console.log(fonction.chiffrerChaine(chaine));
+console.log(chiffrement.chiffrerChaine(chaine));
 
 //Test du troisième fonction chiffrement d'un Relevé d'Identité Bancaire
 const rib = prompt("Saisir une Relevé d'Identité Bancaire :");
-console.log(fonction.chiffrerRib(rib));
+console.log(chiffrement.chiffrerRib(rib));
